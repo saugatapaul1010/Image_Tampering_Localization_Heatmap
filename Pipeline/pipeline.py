00e9c350150b8c02a646f650d48769db24be27f3
Saugata Paul <saugata.paul1010@gmail.com>
2019-10-16 15:19:23 +0530

    Change left for main execution

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -39,6 +39,7 @@ global folder
 def folder_name(directory):
     folder = directory
     print(folder)
+    
 files = os.listdir(folder)
 hdf5_path = 'test_imgs_v2.hdf5'
 
@@ -468,11 +469,11 @@ with tf.Session(config=config) as sess:
         plt.colorbar(probability_graph)
         plt.show()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Enter the directory name where you have the test data')
-    parser.add_argument('--dir', default='test_images', help='default directory name for the test images')
+    parser.add_argument('--dir', default='test_images/', help='default directory name for the test images')
     args = parser.parse_args()
 
     print("INSIDE MAIN")
-
+    folder_name()
     folder_name(args.dir)
